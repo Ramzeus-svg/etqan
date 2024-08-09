db0a7d806800056c2393563d9e6f36ef95aa6e36
Mohamed Ramzy <71365254+Ramzeus-svg@users.noreply.github.com>
2024-08-09 20:59:01 +0300

    UI/UX Modifications

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:Etqan/main.dart';
 
 void main() {
+  setUpAll(() async {
+    // Initialize Firebase for testing
+    TestWidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  });
+
   testWidgets('HomePage displays correctly', (WidgetTester tester) async {
-    // Build the app with a properly initialized MyHomePage.
+    // Build the app with a properly initialized MyApp
     await tester.pumpWidget(const MyApp(homePage: MyHomePage()));
 
     // Ensure any async operations (like Firebase initialization) are completed.
